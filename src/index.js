927eb7ee794ac0a273f7fa2e86bc3041e6d3d88d
Mick Maratta <mickmaratta@gmail.com>
2022-10-15 18:46:07 -0400

    Uploaded info

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext";
+import App from "./App";
+import { StaticRouter } from "react-router-dom/server";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

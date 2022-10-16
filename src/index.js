0f9213134c41efc7cafc6d3ff377ace6b8d51089
Mick Maratta <mickmaratta@gmail.com>
2022-10-16 17:14:57 -0400

    Adjusted Routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext";
 import App from "./App";
-import { StaticRouter } from "react-router-dom/server";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
